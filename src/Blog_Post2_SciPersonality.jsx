091e2502ef4a4d307a8cbc746a8ab92c026bcc72
Katoa Price-Ahau <katoapahau@katoas-air.lan>
2025-07-10 09:59:07 -0400

    Format blog post with line breaks

--- src/Blog_Post2_SciPersonality.jsx
+++ src/Blog_Post2_SciPersonality.jsx
@@ -11,20 +11,20 @@ export default function Blog_Post2_SciPersonality() {
       </p>
 
       <p className="mb-8">
-      The American Psychological Association defines Personality as: "The enduring configuration of characteristics and behavior that comprises an individual's unique adjustment to life, including major traits, interests, drives, values, self-concept, abilities, and emotional patterns."
+      The American Psychological Association defines Personality as: "The enduring configuration of characteristics and behavior that comprises an individual's unique adjustment to life, including major traits, interests, drives, values, self-concept, abilities, and emotional patterns."<br /><br />
+      
+      Put plainly - Personality refers to the patterns of thoughts, feelings and behaviors that distinguishes you from other people. It encompasses how you interact with your environment, people around you and within yourself. It's part internal and part external.<br /><br />
 
-Put plainly - Personality refers to the patterns of thoughts, feelings and behaviors that distinguishes you from other people. It encompasses how you interact with your environment, people around you and within yourself. It's part internal and part external.
-
-Internal: how you personally think and feel, what your general outlook on happenings in the world are and what you choose to take interests in.
+Internal: how you personally think and feel, what your general outlook on happenings in the world are and what you choose to take interests in.<br />
 External: how you filter your internal feelings into external actions toward a person, thing, situation or how you spend your time.
       </p>
 
       <h2 className="text-2xl font-semibold mt-10 mb-4">Can Personality Change?</h2>
 
       <p className="mb-4">
-      "The APA's definition says ""enduring"", so does that mean my personality cannot change? Absolutely not.
+      "The APA's definition says ""enduring"", so does that mean my personality cannot change? Absolutely not.<br /><br />
 
-Personality is not fixed and can evolve over time. A study conducted at UC, Berkeley (published in the Journal of Personality and Social Psychology), found that over time as we age personality domonstrably changes.
+Personality is not fixed and can evolve over time. A study conducted at UC, Berkeley (published in the Journal of Personality and Social Psychology), found that over time as we age personality domonstrably changes.<br /><br />
 
 The study looked at the ""big five"" traits of personality: Conscientiousness, Agreeableness, Neuroticism, Openness and Extraversion. The study found that after age 30, we normally see personality changes within individuals - for the better!"
       </p>
@@ -40,11 +40,11 @@ The study looked at the ""big five"" traits of personality: Conscientiousness, A
       </p>
 
       <p className="mb-8">
--Conscientiousness: increased throughout age ranges with largest change happening in a person's 20s
--Agreeableness: continued to increase from 30-60
--Neuroticism: decreased with age, past young adulthood
--Openness: surprisingly decreased slightly over time, but most likely due to the lack of desire to form new relationships
--Extraversion: varied, but mostly in-line with openness as people tend to want to stick to the relationships they have as they age
+-Conscientiousness: increased throughout age ranges with largest change happening in a person's 20s<br />
+-Agreeableness: continued to increase from 30-60<br />
+-Neuroticism: decreased with age, past young adulthood<br />
+-Openness: surprisingly decreased slightly over time, but most likely due to the lack of desire to form new relationships<br />
+-Extraversion: varied, but mostly in-line with openness as people tend to want to stick to the relationships they have as they age<br />
       </p>
 
       <h2 className="text-2xl font-semibold mt-10 mb-4">
@@ -68,8 +68,7 @@ The study looked at the ""big five"" traits of personality: Conscientiousness, A
       <p className="mb-6">
       Personality and mental age share similarities and learning one can inform the other.
 
-Try out our Mental Age Test for yourself.
- <a href="https://mental-agetest.com/" className="text-blue-600 underline">Try out a Mental Age Test for yourself.</a>
+ <a href="https://mental-agetest.com/" className="text-blue-600 underline">Try out out Mental Age Test for yourself.</a>
       </p>
 
       <p className="text-sm text-gray-500 mt-8">
